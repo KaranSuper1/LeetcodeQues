98ba6fef8da5844c204c1a05384cda82d093d212
Karan Deep Bajaj <karandeep.bajaj@blueyonder.com>
2024-09-05 15:29:24 +0530

    ok

---
+++ Q1_Two Sum.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for k in range(len(nums)-1):
+            for i in range(k,len(nums)-1):
+                if nums[k] + nums[i + 1] == target:
+                    return [i, i + 1]
+
+
+a=Solution()
+print(a.twoSum([3,2,4],6))
